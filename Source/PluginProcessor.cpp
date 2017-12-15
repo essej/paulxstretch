dd9b7f3be350632484b44f95472490a6d7b0b5d8
xenakios <xenakios@gmail.com>
2017-12-15 04:34:22 +0200

    Some skew for low and high pass filter params

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -112,8 +112,10 @@ PaulstretchpluginAudioProcessor::PaulstretchpluginAudioProcessor()
 	addParameter(new AudioParameterFloat("octavemix2_0", "2 octaves up level", 0.0f, 1.0f, 0.0f)); // 20
 	addParameter(new AudioParameterFloat("tonalvsnoisebw_0", "Tonal vs Noise BW", 0.74f, 1.0f, 0.74f)); // 21
 	addParameter(new AudioParameterFloat("tonalvsnoisepreserve_0", "Tonal vs Noise preserve", -1.0f, 1.0f, 0.5f)); // 22
-	addParameter(new AudioParameterFloat("filter_low_0", "Filter low", 20.0f, 10000.0f, 20.0f)); // 23
-	addParameter(new AudioParameterFloat("filter_high_0", "Filter high", 20.0f, 20000.0f, 20000.0f)); // 24
+	addParameter(new AudioParameterFloat("filter_low_0", "Filter low",
+                                         NormalisableRange<float>(20.0f, 10000.0f, 1.0f, 0.3), 20.0f)); // 23
+	addParameter(new AudioParameterFloat("filter_high_0", "Filter high",
+                                         NormalisableRange<float>(100.0f, 20000.0f, 1.0f, 0.3), 20000.0f));; // 24
 	addParameter(new AudioParameterFloat("onsetdetect_0", "Onset detection", 0.0f, 1.0f, 0.0f)); // 25
 	addParameter(new AudioParameterBool("capture_enabled0", "Capture", false)); // 26
 	m_outchansparam = new AudioParameterInt("numoutchans0", "Num output channels", 2, 8, 2); // 27
