13e39ca30c03f2bf9a7b7637316b204855cac7c0
xenakios <xenakios@gmail.com>
2019-01-08 17:08:02 +0200

    Add Steinberg required text in About dialog and update Xenakios copyright year.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -590,12 +590,16 @@ void PaulstretchpluginAudioProcessorEditor::showAbout()
 #ifdef JUCE_DEBUG
 	title += " (DEBUG)";
 #endif
+	String vstInfo;
+	if (processor.wrapperType == AudioProcessor::wrapperType_VST ||
+		processor.wrapperType == AudioProcessor::wrapperType_VST3)
+		vstInfo = "VST Plug-In Technology by Steinberg.\n\n";
 	PluginHostType host;
 	AlertWindow::showMessageBoxAsync(AlertWindow::InfoIcon,
 		title,
 		"Plugin for extreme time stretching and other sound processing\nBuilt on " + String(__DATE__) + " " + String(__TIME__) + "\n"
 		"Copyright (C) 2006-2011 Nasca Octavian Paul, Tg. Mures, Romania\n"
-		"(C) 2017-2018 Xenakios\n\n"
+		"(C) 2017-2019 Xenakios\n\n"+vstInfo+
 		"Using " + fftlib + " for FFT\n\n"
 		+ juceversiontxt + " (c) Roli. Used under the GPL license.\n\n"
 		"GPL licensed source code for this plugin at : https://bitbucket.org/xenakios/paulstretchplugin/overview\n"
