681dc4de6b2d6ef31d0f0496f721f75ff41ee0b0
Jesse Chappell <jesse@sonosaurus.com>
2022-04-15 10:54:39 -0400

    add missing file

---
+++ Source/CrossPlatformUtilsWindows.cpp
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPLv3-or-later WITH Appstore-exception
+// Copyright (C) 2020 Jesse Chappell
+
+
+#include "CrossPlatformUtils.h"
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+#if JUCE_WINDOWS
+
+
+
+void getSafeAreaInsets(void * component, float & top, float & bottom, float & left, float & right, int& notchPos)
+{
+    top = bottom = left = right = notchPos = 0;
+}
+
+#endif
